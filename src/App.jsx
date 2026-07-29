23f3447c22c01c525a50bf3362d8bf659733da87
Melissa Marcaida <melmarcaida1@gmail.com>
2026-07-29 17:45:33 +0800

    remove background

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,11 @@ function App() {
 
       <div className="border grid grid-cols-2 gap-4 h-12 text-l font-semibold">
 
-        <div className="flex justify-center border items-center">
+        <div className="flex justify-center items-center">
           <button className="rounded-full h-8 w-50 hover:bg-gray-300">Home</button>
         </div>
 
-        <div className="border bg-red-100 flex items-center gap-4 sm:gap-14">
+        <div className="flex items-center gap-4 sm:gap-14">
           <NavButton>About</NavButton>
           <NavButton>Projects</NavButton>
           <NavButton>Contact</NavButton> 
@@ -27,11 +27,11 @@ function App() {
       </div>
 
 
-      <div className="flex mx-auto  justify-center items-center bg-yellow-100 h-45 flex-row items-center justify-center gap-4">
+      <div className="flex mx-auto  justify-center items-center h-45 flex-row items-center justify-center gap-4">
         <div className="md:shrink-0">
           <img className="object-cover block h-32 min-w-[8rem] w-32 rounded-full " src={mel} alt="MM" />
         </div>
-        <div className="bg-red-100 justify-center items-center ">
+        <div className=" justify-center items-center ">
           <p className="text-xl font-semibold">Melissa D. Marcaida</p>
           <p className="text-sm">Aspiring Software Engineer</p>
           <p className="text-xs">Currently a 2nd year student at Polytechnic University of the Philippines</p>
@@ -39,14 +39,14 @@ function App() {
       </div>
 
 
-        <div className="mx-auto text-center bg-red-200 text-l font-semibold flex justify-center items-center">
+        <div className="mx-auto text-center text-l font-semibold flex justify-center items-center">
           <p className="text-xs ">A self taught ambitious b*tch with a passion in software development. Still a NOOB in everything yet exploring C++, Javascript, and React.</p>
         </div>
 
         <div>
-          <p className="bg-orange-200 mx-auto text-center font-semibold p-2 mt-6">Projects</p>
+          <p className="border mx-auto text-center font-semibold p-2 mt-6">Projects</p>
 
-          <div className="flex mx-auto justify-center items-center bg-yellow-100 flex-col items-center justify-center gap-4">
+          <div className="flex mx-auto justify-center items-center flex-col items-center justify-center gap-4 mt-4">
 
             <Project description="Description of Project 1" image="Image 1">Project 1</Project>
             <Project description="Description of Project 2" image="Image 2">Project 2</Project>
@@ -56,7 +56,7 @@ function App() {
         </div>
 
         <div className="mx-auto text-center sm:text-left text-l max-w-md font-semibold flex justify-center items-center mt-6">
-          <p className="bg-blue-200 text-sm">At my stage, I'm still exploring, experimenting, 
+          <p className="text-sm">At my stage, I'm still exploring, experimenting, 
             and struggling through every project. I believe it's worth losing my mind over rather
             than being trapped in a prison of comfort. XD
           </p>
@@ -65,10 +65,10 @@ function App() {
         <hr className="mx-auto max-w-md mt-2 border-1 border-black" />
 
         <div className="mx-auto text-center justify-center items-center max-w-md flex justify-center items-center mt-2">
-          <p className="bg-green-200 text-l font-semibold mx-auto">Have a project in mind? Let's connect!</p>
+          <p className=" text-l font-semibold mx-auto">Have a project in mind? Let's connect!</p>
         </div>
 
-        <div className="mx-auto max-w-md bg-red-200 flex justify-center items-center flex-row gap-6 items-center mt-4">
+        <div className="mx-auto max-w-md flex justify-center items-center flex-row gap-6 items-center mt-4">
           <div className="w-12 h-12 rounded-full flex justify-center items-center ">
             <TiSocialFacebookCircular className="text-6xl" />
           </div>
