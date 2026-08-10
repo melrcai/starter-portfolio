d2edfeeddcb5bd02bd17b96fd2e179d84d790150
Melissa Marcaida <melmarcaida1@gmail.com>
2026-08-10 23:01:45 +0800

    feat: make contact links clickable

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,24 @@ import { IoLogoInstagram } from "react-icons/io";
 import { FaLinkedin } from "react-icons/fa";
 
 function Contact () {
+  const socials = [
+    {
+      label: 'Facebook',
+      href: 'https://www.facebook.com/melmarcaidaa/',
+      icon: <TiSocialFacebookCircular className="text-6xl" />,
+    },
+    {
+      label: 'Instagram',
+      href: 'https://www.instagram.com/mxlrcai/',
+      icon: <IoLogoInstagram className="text-6xl" />,
+    },
+    {
+      label: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/melissa-marcaida/',
+      icon: <FaLinkedin className="text-4xl" />,
+    },
+  ]
+
   return (
     <section id="contact" className="pt-4 pb-8">
         <div className="mx-auto text-center max-w-md flex justify-center items-center mt-2">
@@ -10,15 +28,18 @@ function Contact () {
         </div>
 
         <div className="mx-auto max-w-md flex justify-center flex-row gap-6 items-center mt-4">
-          <div className="hover:bg-gray-500/30 active:bg-gray-500/50 duration-300 w-12 h-12 rounded-full flex justify-center items-center ">
-            <TiSocialFacebookCircular className="text-6xl" />
-          </div>
-          <div className="hover:bg-gray-500/30 active:bg-gray-500/50 duration-300 w-12 h-12 rounded-full flex justify-center items-center ">
-            <IoLogoInstagram className="text-6xl" />
-          </div>
-          <div className="hover:bg-gray-500/30 active:bg-gray-500/50 duration-300 w-12 h-12 rounded-full flex justify-center items-center ">
-            <FaLinkedin className="text-4xl" />
-          </div>
+          {socials.map((social) => (
+            <a
+              key={social.label}
+              href={social.href}
+              target="_blank"
+              rel="noreferrer"
+              aria-label={social.label}
+              className="hover:bg-gray-500/30 active:bg-gray-500/50 duration-300 w-12 h-12 rounded-full flex justify-center items-center"
+            >
+              {social.icon}
+            </a>
+          ))}
         </div>
     </section>
   )
